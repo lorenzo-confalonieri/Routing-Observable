38638f19f664b42ec8d8af4d1fed6fb9f1a6919a
lorenzo-confalonieri <confalonieri.lorenzo@iisgalvanimi.edu.it>
2026-05-02 10:52:37 +0000

    primo commit

---
+++ mio-primo-sito/src/app/animals-component/animals-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animals-component',
+  imports: [],
+  templateUrl: './animals-component.html',
+  styleUrl: './animals-component.css',
+})
+export class AnimalsComponent {}
---
+++ mio-primo-sito/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('mio-primo-sito');
+}
